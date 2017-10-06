126fa77a8c56af1a216a024c148f2553183c6912
Andrew Cookro <andrew.cookro@gmail.com>
2017-10-06 17:24:11 -0400

    Add smooth scrolling to navigation

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-
+// Click handler for menu
 
   $('.menu-toggle').click(function() {
     $('.nav-menu').toggleClass('nav-menu--open', 500);
     $(this).toggleClass('open');
   })
+
+
+// Smooth Scrolling
+
+  // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 
